59353a869f54d4ee44e48c02d9b68d0192dc592e
Neyamul Haque <omy@eagle3dstreaming.com>
2024-01-30 00:14:27 +0600

    removed extra functions

--- js/allin1.js
+++ js/allin1.js
@@ -14,112 +14,6 @@ websocketUrl="wss://s10.eaglepixelstreaming.com"
 // This code is referenced from webrtc sample.
 // https://github.com/webrtc/samples/blob/gh-pages/src/content/peerconnection/trickle-ice/js/main.js
 
-const servers = document.querySelector('select#servers');
-const urlInput = document.querySelector('input#url');
-const usernameInput = document.querySelector('input#username');
-const passwordInput = document.querySelector('input#password');
-
-const allServersKey = 'servers';
-
-//export
-function addServer() {
- const scheme = urlInput.value.split(':')[0];
- if (!['stun', 'stuns', 'turn', 'turns'].includes(scheme)) {
-   alert(`URI scheme ${scheme} is not valid`);
-   return;
- }
-
- // Store the ICE server as a stringified JSON object in option.value.
- const option = document.createElement('option');
- const iceServer = {
-   urls: [urlInput.value],
-   username: usernameInput.value,
-   credential: passwordInput.value
- };
- option.value = JSON.stringify(iceServer);
- option.text = `${urlInput.value} `;
- const username = usernameInput.value;
- const password = passwordInput.value;
- if (username || password) {
-   option.text += (` [${username}:${password}]`);
- }
- option.ondblclick = selectServer;
- servers.add(option);
- urlInput.value = usernameInput.value = passwordInput.value = '';
- writeServersToLocalStorage();
-}
-
-//export 
-function removeServer() {
- for (let i = servers.options.length - 1; i >= 0; --i) {
-   if (servers.options[i].selected) {
-     servers.remove(i);
-   }
- }
- writeServersToLocalStorage();
-}
-
-//export 
-
-function reset() {
- window.localStorage.clear();
- document.querySelectorAll('select#servers option').forEach(option => option.remove());
- const serversSelect = document.querySelector('select#servers');
- setDefaultServer(serversSelect);
-}
-
-function selectServer(event) {
- const option = event.target;
- const value = JSON.parse(option.value);
- urlInput.value = value.urls[0];
- usernameInput.value = value.username || '';
- passwordInput.value = value.credential || '';
-}
-
-function setDefaultServer(serversSelect) {
- const option = document.createElement('option');
- option.value = '{"urls":["stun:stun.l.google.com:19302"]}';
- option.text = 'stun:stun.l.google.com:19302';
- option.ondblclick = selectServer;
- serversSelect.add(option);
-}
-
-function writeServersToLocalStorage() {
- const serversSelect = document.querySelector('select#servers');
- const allServers = JSON.stringify(Object.values(serversSelect.options).map(o => JSON.parse(o.value)));
- window.localStorage.setItem(allServersKey, allServers);
-}
-
-//export 
-function readServersFromLocalStorage() {
- document.querySelectorAll('select#servers option').forEach(option => option.remove());
- const serversSelect = document.querySelector('select#servers');
- const storedServers = window.localStorage.getItem(allServersKey);
-
- if (storedServers === null || storedServers === '') {
-   setDefaultServer(serversSelect);
- } else {
-   JSON.parse(storedServers).forEach((server) => {
-     const o = document.createElement('option');
-     o.value = JSON.stringify(server);
-     o.text = server.urls[0];
-     o.ondblclick = selectServer;
-     serversSelect.add(o);
-   });
- }
-}
-
-//export 
-function getServers() {
- const storedServers = window.localStorage.getItem(allServersKey);
-
- if (storedServers === null || storedServers === '') {
-   return [{ urls: ['stun:stun.l.google.com:19302'] }];
- }
- else {
-   return JSON.parse(storedServers);
- }
-}
 
 //export 
 async function getServerConfig() {
@@ -132,7 +26,7 @@ async function getServerConfig() {
 function getRTCConfiguration() {
  let config = {};
  config.sdpSemantics = 'unified-plan';
- config.iceServers = getServers();
+ config.iceServers = [{ urls: ['stun:stun.l.google.com:19302'] }];
  return config;
 }
 
@@ -145,91 +39,7 @@ function getRTCConfiguration() {
 * @return {Array<string>} - display string Array
 */
 //export 
-function createDisplayStringArray(report, lastReport) {
- let array = new Array();
 
- report.forEach(stat => {
-   if (stat.type === 'inbound-rtp') {
-     array.push(`${stat.kind} receiving stream stats`);
-
-     if (stat.codecId != undefined) {
-       const codec = report.get(stat.codecId);
-       array.push(`Codec: ${codec.mimeType}`);
-
-       if (codec.sdpFmtpLine) {
-         codec.sdpFmtpLine.split(";").forEach(fmtp => {
-           array.push(` - ${fmtp}`);
-         });
-       }
-
-       if (codec.payloadType) {
-         array.push(` - payloadType=${codec.payloadType}`);
-       }
-
-       if (codec.clockRate) {
-         array.push(` - clockRate=${codec.clockRate}`);
-       }
-
-       if (codec.channels) {
-         array.push(` - channels=${codec.channels}`);
-       }
-     }
-
-     if (stat.kind == "video") {
-       array.push(`Decoder: ${stat.decoderImplementation}`);
-       array.push(`Resolution: ${stat.frameWidth}x${stat.frameHeight}`);
-       array.push(`Framerate: ${stat.framesPerSecond}`);
-     }
-
-     if (lastReport && lastReport.has(stat.id)) {
-       const lastStats = lastReport.get(stat.id);
-       const duration = (stat.timestamp - lastStats.timestamp) / 1000;
-       const bitrate = (8 * (stat.bytesReceived - lastStats.bytesReceived) / duration) / 1000;
-       array.push(`Bitrate: ${bitrate.toFixed(2)} kbit/sec`);
-     }
-   } else if (stat.type === 'outbound-rtp') {
-     array.push(`${stat.kind} sending stream stats`);
-
-     if (stat.codecId != undefined) {
-       const codec = report.get(stat.codecId);
-       array.push(`Codec: ${codec.mimeType}`);
-
-       if (codec.sdpFmtpLine) {
-         codec.sdpFmtpLine.split(";").forEach(fmtp => {
-           array.push(` - ${fmtp}`);
-         });
-       }
-
-       if (codec.payloadType) {
-         array.push(` - payloadType=${codec.payloadType}`);
-       }
-
-       if (codec.clockRate) {
-         array.push(` - clockRate=${codec.clockRate}`);
-       }
-
-       if (codec.channels) {
-         array.push(` - channels=${codec.channels}`);
-       }
-     }
-
-     if (stat.kind == "video") {
-       array.push(`Encoder: ${stat.encoderImplementation}`);
-       array.push(`Resolution: ${stat.frameWidth}x${stat.frameHeight}`);
-       array.push(`Framerate: ${stat.framesPerSecond}`);
-     }
-
-     if (lastReport && lastReport.has(stat.id)) {
-       const lastStats = lastReport.get(stat.id);
-       const duration = (stat.timestamp - lastStats.timestamp) / 1000;
-       const bitrate = (8 * (stat.bytesSent - lastStats.bytesSent) / duration) / 1000;
-       array.push(`Bitrate: ${bitrate.toFixed(2)} kbit/sec`);
-     }
-   }
- });
-
- return array;
-}
 
 //import { VideoPlayer } from "../../js/videoplayer.js";
 //import { Observer, Sender } from "../module/sender.js";
@@ -2816,7 +2626,6 @@ function onClickPlayButton() {
 }
 
 async function setupRenderStreaming() {
-
  const signaling = useWebSocket ? new WebSocketSignaling() : new Signaling();
  const config = getRTCConfiguration();
  renderstreaming = new RenderStreaming(signaling, config);
@@ -2830,7 +2639,7 @@ async function setupRenderStreaming() {
 function onConnect() {
  const channel = renderstreaming.createDataChannel("input");
  videoPlayer.setupInput(channel);
- showStatsMessage();
+
 }
 
 async function onDisconnect(connectionId) {
@@ -2849,26 +2658,3 @@ let lastStats;
 /** @type {number} */
 let intervalId;
 
-function showStatsMessage() {
- intervalId = setInterval(async () => {
-   if (renderstreaming == null) {
-     return;
-   }
-
-   const stats = await renderstreaming.getStats();
-   if (stats == null) {
-     return;
-   }
-
-   const array = createDisplayStringArray(stats, lastStats);
-   lastStats = stats;
- }, 1000);
-}
-
-function clearStatsMessage() {
- if (intervalId) {
-   clearInterval(intervalId);
- }
- lastStats = null;
- intervalId = null;
-}
